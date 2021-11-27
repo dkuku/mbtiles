2ea2d9fe16578a70710af806b41f9359edf4c480
Daniel Kukula <daniel.kuku@gmail.com>
2021-11-27 11:49:41 +0000

    add json files

--- lib/mbtiles/helpers.ex
+++ lib/mbtiles/helpers.ex
@@ -1,4 +1,9 @@
 defmodule Mbtiles.Helpers do
   @moduledoc false
   def zoom_level_to_tile_size(zoom), do: 40_000_000 / :math.pow(2, zoom)
+
+  def get_priv_path(filename) do
+    [ :code.priv_dir(:mbtiles), filename]
+    |> Path.join()
+  end
 end
